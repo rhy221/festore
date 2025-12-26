e840ac6e5e176f7b0cb1f8e746a11437ab529788
rhy221 <ghuy9366@gmail.com>
2025-12-27 00:34:18 +0700

    fix: fix bugs

--- apps/designer/app/auth/login/page.tsx
+++ apps/designer/app/auth/login/page.tsx
@@ -1,9 +1,19 @@
 import LoginForm from "@/components/auth/login-form";
+import { Spinner } from "@workspace/ui/components/spinner";
+import { Suspense } from "react";
+
+export const dynamic = 'force-dynamic';
 
 export default function LoginPage() {
-  return (
+   return (
     <div className="w-full max-w-xs">
-      <LoginForm />
+      <Suspense fallback={
+        <div className="flex justify-center p-4">
+          <Spinner />
+        </div>
+      }>
+        <LoginForm/>
+      </Suspense>
     </div>
   );
 }
