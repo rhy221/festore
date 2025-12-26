176af27c03a56773d954fdd80ef41472428c4881
rhy221 <ghuy9366@gmail.com>
2025-12-26 22:52:31 +0700

    fix: fix bugs

--- apps/admin/queries/useAuction.ts
+++ apps/admin/queries/useAuction.ts
@@ -1,31 +1,31 @@
-import { auctionAction } from "@/api/home.api";
-import { GetAuctionBidsResType, GetAuctionResType, GetAuctionsResType } from "@/schema/auction.schema";
-import { useMutation, useQuery } from "@tanstack/react-query"
+// import { auctionAction } from "@/api/home.api";
+// import { GetAuctionBidsResType, GetAuctionResType, GetAuctionsResType } from "@/schema/auction.schema";
+// import { useMutation, useQuery } from "@tanstack/react-query"
 
-export const useAuctionQuery = (auctionId: string) => {
-    return useQuery<GetAuctionResType>({
-        queryKey: ["auction"],
-        queryFn: () => auctionAction.getAuction(auctionId),
-    });
-}
+// export const useAuctionQuery = (auctionId: string) => {
+//     return useQuery<GetAuctionResType>({
+//         queryKey: ["auction"],
+//         queryFn: () => auctionAction.getAuction(auctionId),
+//     });
+// }
 
-export const useAuctionsQuery = (filter: string) => {
-    return useQuery<GetAuctionsResType>({
-        queryKey: ["auctions", filter],
-        queryFn: () => auctionAction.getAuctions(filter),
-        gcTime: 0,
-    });
-}
+// export const useAuctionsQuery = (filter: string) => {
+//     return useQuery<GetAuctionsResType>({
+//         queryKey: ["auctions", filter],
+//         queryFn: () => auctionAction.getAuctions(filter),
+//         gcTime: 0,
+//     });
+// }
 
-export const useAuctionBidsQuery = (auctionId: string) => {
-    return useQuery<GetAuctionBidsResType>({
-        queryKey: ["auctionBids"],
-        queryFn: () => auctionAction.getAuctionBids(auctionId),
-    });
-}
+// export const useAuctionBidsQuery = (auctionId: string) => {
+//     return useQuery<GetAuctionBidsResType>({
+//         queryKey: ["auctionBids"],
+//         queryFn: () => auctionAction.getAuctionBids(auctionId),
+//     });
+// }
 
-export const usePlaceAuctionBidMutation = () => {
-    return useMutation({
-        mutationFn: auctionAction.placeAuctionBid
-    });
-}
+// export const usePlaceAuctionBidMutation = () => {
+//     return useMutation({
+//         mutationFn: auctionAction.placeAuctionBid
+//     });
+// }
--- apps/admin/queries/useDashboard.tsx
+++ apps/admin/queries/useDashboard.tsx
@@ -1,28 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
-import { dashboardApi } from "../api/reports.api";
+// import { useQuery } from "@tanstack/react-query";
+// import { dashboardApi } from "../api/reports.api";
 
-export const useDashboardQueries = {
-  useMonthlyReport: (month: string) => {
-    return useQuery({
-      queryKey: ["monthlyReport", month],
-      queryFn: () => dashboardApi.getMonthlyReport(month),
-      staleTime: 5 * 60 * 1000, // 5 minutes
-    });
-  },
+// export const useDashboardQueries = {
+//   useMonthlyReport: (month: string) => {
+//     return useQuery({
+//       queryKey: ["monthlyReport", month],
+//       queryFn: () => dashboardApi.getMonthlyReport(month),
+//       staleTime: 5 * 60 * 1000, // 5 minutes
+//     });
+//   },
 
-  useCategoryAnalysis: () => {
-    return useQuery({
-      queryKey: ["categoryAnalysis"],
-      queryFn: () => dashboardApi.getCategoryAnalysis(),
-      staleTime: 5 * 60 * 1000,
-    });
-  },
+//   useCategoryAnalysis: () => {
+//     return useQuery({
+//       queryKey: ["categoryAnalysis"],
+//       queryFn: () => dashboardApi.getCategoryAnalysis(),
+//       staleTime: 5 * 60 * 1000,
+//     });
+//   },
 
-  useSalesHistory: () => {
-    return useQuery({
-      queryKey: ["salesHistory"],
-      queryFn: () => dashboardApi.getSalesHistory(),
-      staleTime: 2 * 60 * 1000, // 2 minutes
-    });
-  },
-};
+//   useSalesHistory: () => {
+//     return useQuery({
+//       queryKey: ["salesHistory"],
+//       queryFn: () => dashboardApi.getSalesHistory(),
+//       staleTime: 2 * 60 * 1000, // 2 minutes
+//     });
+//   },
+// };
