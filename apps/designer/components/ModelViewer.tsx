a6a591bc57b97d00919b37618df0ed63430a9857
rhy221 <ghuy9366@gmail.com>
2025-12-26 21:42:49 +0700

    fix: fix bugs

--- apps/designer/components/ModelViewer.tsx
+++ apps/designer/components/ModelViewer.tsx
@@ -1,20 +1,20 @@
-// components/ModelViewer.tsx
-import dynamic from "next/dynamic";
+// // components/ModelViewer.tsx
+// import dynamic from "next/dynamic";
 
-const ModelViewer = dynamic(
-  () => import("@google/model-viewer").then(() => () => <model-viewer />),
-  { ssr: false }
-);
+// const ModelViewer = dynamic(
+//   () => import("@google/model-viewer").then(() => () => <model-viewer />),
+//   { ssr: false }
+// );
 
-export default function DisplayModelViewer({ src }: { src: string }) {
-  return (
-    <ModelViewer
-      src={src}
-      alt="3D model"
-      auto-rotate
-      camera-controls
-      ar
-      style={{ width: "500px", height: "500px" }}
-    />
-  );
-}
+// export default function DisplayModelViewer({ src }: { src: string }) {
+//   return (
+//     <ModelViewer
+//       src={src}
+//       alt="3D model"
+//       auto-rotate
+//       camera-controls
+//       ar
+//       style={{ width: "500px", height: "500px" }}
+//     />
+//   );
+// }
