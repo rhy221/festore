f5d1a1004cc9fa992f6e539d61662e09a2ab0054
rhy221 <ghuy9366@gmail.com>
2025-12-28 22:17:31 +0700

    fix: vercel deploy bugs

--- apps/designer/api/auction.api.ts
+++ apps/designer/api/auction.api.ts
@@ -1,4 +1,4 @@
-import http from "@/lib/http";
+import http from "../lib/http";
 import { AuctionType, GetAuctionBidsResType, GetAuctionResType, GetAuctionsResType, PlaceAcutionBidResType, PlaceAuctionBidBodyType } from "@/schemas/auction.schema";
 
 
